794436967f25f626c6cf5f8827bf942aeed8e05b
Karla Cervantes <cervantes.karla14@gmail.com>
2020-12-03 18:41:59 -0600

    tasks almost complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,25 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+import axios from "axios";
+// console.log(axios);
+// const result = axios.get("https://api.github.com/users/kmcervan");
+// console.log(result);
+console.log('1 getting Data');
 
+axios
+.get('https://api.github.com/users/kmcervan')
+.then((res) => {
+    console.log('2 here is the future data', futureData);
+    const images = futureData.data.message;
+    console.log((images) => {
+      const hubMaker = newMaker({ })
+    })
+  })
+  .catch(error => {
+    console.log(drama);
+  })
+  console.log('3 Data requested');
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -30,6 +48,56 @@
 
 const followersArray = [];
 
+
+
+/*
+  List of LS Instructors Github username's:
+    tetondan
+    dustinmyers
+    justsml
+    luishrd
+    bigknell
+*/
+function hubMaker (data){
+  const card = document.createElement('div');
+  const img = document.createElement('img')
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const a = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+  card.appendChild(name);
+  card.appendChild(userName);
+  card.appendChild(location);
+  card.appendChild(profile);
+  card.appendChild(a);
+  card.appendChild(followers);
+  card.appendChild(following);
+  card.appendChild(bio);
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+  
+  img.src = `${'https://avatars2.githubusercontent.com/u/72095687?v=4'}`;
+  name.textContent = `${res.data.name}`;
+  userName.textContent = `${data.login}`;
+  location.textContent = `${data.location}`;
+  profile.textContent = 'Profile:';
+  a.href = `${'https://api.github.com/users/kmcervan'}`;
+  followers.textContent = `${data.followers}`;
+  following.textContent = `${data.following}`;
+  bio.textContent = `${data.bio}`;
+}
+
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -48,13 +116,4 @@ const followersArray = [];
         <p>Bio: {users bio}</p>
       </div>
     </div>
-*/
-
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
 */
